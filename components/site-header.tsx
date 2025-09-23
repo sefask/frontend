0ec3f70421cd07077b3fc787b05049110be88626
xCHLOMIx <gutabarwaa@gmail.com>
2025-09-23 15:14:19 +0200

    MODIFIED: The site header to only show sidebar toggle icon when on smaller screens

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -20,16 +20,16 @@ export function SiteHeader() {
 
   return (
     <header className="bg-background sticky top-0 z-50 flex w-full items-center border-b">
-      <div className="flex h-(--header-height) w-full items-center gap-2 px-4">
+      <div className="flex h-(--header-height) w-full items-center gap-2 px-4 max-md:px-2">
         <Button
-          className="h-8 w-8"
+          className="h-8 w-8 md:hidden"
           variant="ghost"
           size="icon"
           onClick={toggleSidebar}
         >
           <SidebarIcon />
         </Button>
-        <Separator orientation="vertical" className="mr-2 h-4" />
+        <Separator orientation="vertical" className="mr-2 h-4 md:hidden" />
         <Breadcrumb className="hidden sm:block">
           <BreadcrumbList>
             <BreadcrumbItem>
