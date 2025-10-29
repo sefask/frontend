47a8813958592676c91d6da69400e6a6d1062261
xCHLOMIx <gutabarwaa@gmail.com>
2025-10-29 17:29:33 +0200

    ADDED: The backend-url normally to see if the fetch on signup would work

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL}/api/:path*`,
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
